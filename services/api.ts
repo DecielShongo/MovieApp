05a5a65f09cf6cfca295e791acf08051241da471
Deciel <shongo021@gmail.com>
2025-03-31 20:33:46 +0200

    added TMDB Api

---
+++ services/api.ts
@@ -0,0 +1,27 @@
+export const TMDB_CONFIG = {
+    API_KEY: process.env.EXPO_PUBLIC_API_KEY,
+    BASE_URL: 'https://api.themoviedb.org/3/',
+    headers: {
+        Authorization: `Bearer ${process.env.EXPO_PUBLIC_API_KEY}`,
+        accept: 'application/json'
+    }
+}
+
+export const fetchMovies = async ({query} : {query: string}) => {
+    const endpoint = query ? `${TMDB_CONFIG.BASE_URL}discover/movie?sort_by=popularity.desc` : `${TMDB_CONFIG.BASE_URL}search/movie${encodeURIComponent(query)}`;
+
+    const response = await fetch(endpoint, {
+        method: 'GET',
+        headers: TMDB_CONFIG.headers
+    })
+    if(!response.ok){
+        //@ts-ignore
+    throw new Error('Something went wrong while fetching movies', response.statusText);
+    }
+
+    const data = await response.json();
+
+    return data.results;
+     
+}
+
